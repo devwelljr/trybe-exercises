787401fbca58bd7f89773018624e50911f9336b8
Wellington <junior.wel02@exemplo.br>
2021-04-22 18:41:10 -0300

    Ate a 5

---
+++ bloco_4/dia_1/exercise.js
@@ -0,0 +1,48 @@
+let a = 100;
+let b = 20;
+let c = 50;
+
+let adição = a + b;
+let trio = a + b + c;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let módulo = a % b;
+
+/* exercicio 2 if (a > b) {
+  console.log('O maior numero é' a);
+}
+  else {
+      console.log('O maior numero é' b);
+  }*/
+
+/* exercicio 3 if (a > b && a > c){
+    console.log('o maior numero é', a);
+}
+else if(b > a && b > c){
+    console.log('o maior numero é', b);
+}
+else{
+    console.log('o maior numero é', c)
+} */
+
+/* exercicio 4 if(a > 0){
+  console.log('positive')
+}
+else if(a < 0){
+  console.log('negative')
+}
+else{
+  console.log(0)
+} */
+
+/* exercicio 5 if(trio === 180){
+  console.log(true)
+}
+else if (trio < 180){
+  console.log(false)
+}
+else{
+  console.log('Erro')
+} */
+
