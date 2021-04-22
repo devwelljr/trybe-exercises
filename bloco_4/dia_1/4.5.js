30c61fbeb409960ebab9cef7d730b9ffc1a16d7e
Wellington <junior.wel02@exemplo.br>
2021-04-22 17:56:48 -0300

    Parte 5

---
+++ bloco_4/dia_1/4.5.js
@@ -0,0 +1,18 @@
+let estado = 'ista';
+
+switch(estado){
+    case 'Aprovada':
+    console.log('Aprovada');
+    break;
+
+    case 'Lista':
+        console.log('Lista')
+        break;
+
+    case 'Reprovada':
+    console.log('Reprovada')
+    break;
+
+    default:
+        console.log('não se aplica')
+}
