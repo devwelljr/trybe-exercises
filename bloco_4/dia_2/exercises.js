8ce53afeb1690bca22876fce4f96a5e45470de55
Wellington <junior.wel02@exemplo.br>
2021-04-23 18:30:10 -0300

    Ate a 3

---
+++ bloco_4/dia_2/exercises.js
@@ -0,0 +1,25 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* 1° exercicio
+for(let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+} */
+
+/* 2° exercicio
+let resultado = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    resultado += numbers[index];
+}
+
+console.log(resultado); */
+
+/*exercicio 3
+ let soma = 0;
+let resultado = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+    resultado = soma / numbers.length;
+}
+
+console.log(resultado); */
