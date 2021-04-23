b73666a612ec8f2fc6cd5e030484a633f57a0ce3
Wellington <junior.wel02@exemplo.br>
2021-04-22 22:21:04 -0300

    Tudo

--- bloco_4/dia_1/4.5.js
+++ bloco_4/dia_1/4.5.js
@@ -1,18 +1,18 @@
-let estado = 'ista';
+let estado = "ista";
 
-switch(estado){
-    case 'Aprovada':
-    console.log('Aprovada');
+switch (estado) {
+  case "Aprovada":
+    console.log("Aprovada");
     break;
 
-    case 'Lista':
-        console.log('Lista')
-        break;
-
-    case 'Reprovada':
-    console.log('Reprovada')
+  case "Lista":
+    console.log("Lista");
     break;
 
-    default:
-        console.log('não se aplica')
+  case "Reprovada":
+    console.log("Reprovada");
+    break;
+
+  default:
+    console.log("não se aplica");
 }
