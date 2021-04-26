8d25df537f2e5e16b5110ae46a86998deb021d69
Wellington <junior.wel02@exemplo.br>
2021-04-26 16:36:25 -0300

    Exercicios 1 e 2

---
+++ bloco_4/dia_3/exercises.js
@@ -0,0 +1,26 @@
+let n = 5;
+let rept = "";
+/* Exercicio 1
+if (n > 0) {
+  for (let index = 0; index < n; index += 1) {
+    rept = rept + "*";
+  }
+
+  for (let indexc = 0; indexc < n; indexc += 1) {
+    console.log(rept)
+  }
+}
+*/
+
+/*exercicio 2
+if (n > 0) {
+  for (let index = 0; index < n; index += 1) {
+    linha = linha + "*";
+    console.log(linha)
+    for (let indexc = 0; indexc < n; indexc += 1) {
+     coluna = coluna + '*'
+    
+    }
+  }
+}
+*/
