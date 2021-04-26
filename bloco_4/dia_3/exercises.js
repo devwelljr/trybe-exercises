e51a088bbbd159b5bf645f3ee250858ac19626b8
Wellington <junior.wel02@exemplo.br>
2021-04-26 20:24:01 -0300

    Exercicios 3 e 4

--- bloco_4/dia_3/exercises.js
+++ bloco_4/dia_3/exercises.js
@@ -1,26 +1,63 @@
 let n = 5;
-let rept = "";
 /* Exercicio 1
+let rept = "";
 if (n > 0) {
   for (let index = 0; index < n; index += 1) {
     rept = rept + "*";
   }
 
   for (let indexc = 0; indexc < n; indexc += 1) {
-    console.log(rept)
+    console.log(rept);
   }
 }
 */
 
 /*exercicio 2
+let linha = '';
+
 if (n > 0) {
   for (let index = 0; index < n; index += 1) {
-    linha = linha + "*";
+    linha += "*";
     console.log(linha)
-    for (let indexc = 0; indexc < n; indexc += 1) {
-     coluna = coluna + '*'
-    
+  }
+}
+*/
+
+/*exercicio 3
+let linha = "";
+let spa = "";
+
+if (n > 0) {
+  for (espaços = 0; espaços < n; espaços += 1) {
+    linha += " ";
+  }
+  for (let index = 0; index < n; index += 1) {
+    linha = linha.substr(1);
+    linha += "*";
+    console.log(linha);
+  }
+}
+*/
+
+/*exercicio 4
+if (n > 0) {
+  let meio = (n + 1) / 2;
+  let left = meio;
+  let right = meio;
+
+  for (let linhaIndex = 0; linhaIndex <= meio; linhaIndex += 1) {
+    let linha = "";
+    for (let colunaIndex = 0; colunaIndex <= n; colunaIndex += 1) {
+      if (colunaIndex > left && colunaIndex < right) {
+        linha += '*';
+      }else{
+        linha += ' ';
+      }
     }
+    console.log(linha)
+    right += 1;
+    left -= 1;
+    
   }
 }
 */
