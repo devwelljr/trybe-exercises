d7a086809de96a5b07ef82642cf32751fbda19ef
Wellington <junior.wel02@exemplo.br>
2021-04-27 15:41:55 -0300

    Exercicios parte 2 for/in

---
+++ bloco_4/dia_4/4.4.2.js
@@ -0,0 +1,20 @@
+// Exercicio 1
+// let names = {
+//   person1: "João",
+//   person2: "Maria",
+//   person3: "Jorge",
+// };
+// for(let index in names){
+//     console.log(`Olá ${names[index]}`)
+// }
+
+// Exercicio 2
+let carro = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
+
+for (let index in carro) {
+  console.log(`${index}, ${carro[index]}`);
+}
