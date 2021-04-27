482c9b767b36151da44faf706883964742887a3f
Wellington <junior.wel02@exemplo.br>
2021-04-27 19:48:00 -0300

    Exercicios da parte 4

---
+++ bloco_4/dia_4/exercises.js
@@ -0,0 +1,63 @@
+//EXERCICIOS PARTE 1
+
+let infoMargarida = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+let infoPatinhas = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+// Exercicio 1
+// let infoMargarida = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   console.log(`Bem-vinda, ${infoMargarita.personagem}.`);
+
+// Exercicio 2
+// console.log(infop.recorrente);
+
+// Exercicio 3
+// for(let index2 in infop){
+//     console.log(index)
+// }
+
+// Exercicio 4
+// for(let index3 in infop){
+//     console.log(infop[index]);
+// }
+
+// Exercicio 5
+// for (let index4 in infoMargarida) {
+//   if (infoMargarida[index4] === infoPatinhas[index4]) {
+//     console.log("Ambos recorrentes");
+//   } else {
+//     console.log(infoPatinhas[index4] + " e " + infoMargarida[index4]);
+//   }
+// }
+
+//EXERCICIOS PARTES 2
+
+//Exercicio 1
+// let palavra = "arara";
+
+// function palindromo(palavra) {
+//   let palavraC = palavra.split("").reverse().join("");
+//   if (palavraC === palavra) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(palindromo(palavra));
+
