cbf2237d6de1c3336d43873eed8dd8173ebdd995
Wellington <junior.wel02@exemplo.br>
2021-04-27 22:13:22 -0300

    Exercicios menos o bonus

--- bloco_4/dia_4/exercises.js
+++ bloco_4/dia_4/exercises.js
@@ -58,6 +58,101 @@ let infoPatinhas = {
 //     return false;
 //   }
 // }
-
 // console.log(palindromo(palavra));
+
+//Exercicio 2
+
+// let array = [2, 3, 6, 7, 10, 1];
+
+// function maiorIndice(array) {
+//   let maior = array[0];
+//   let indice = 0;
+//   for (let index = 1; index < array.length; index += 1) {
+//     if (array[index] > maior) {
+//       indice = index;
+//     }
+//   }
+//   return indice;
+// }
+// console.log(maiorIndice(array));
+
+//Exercicio 3
+
+// let array = [2, 4, 6, 7, 10, 0, -3];
+
+// function menorIndice(array) {
+//   let indice = 0;
+//   for (let index in array) {
+//     if (array[index] < array[indice]) {
+//       indice = index;
+//     }
+//   }
+//   return indice;
+// }
+
+// console.log(menorIndice(array));
+
+//Exercicio 4
+
+// function maisCaracteres(nomes){
+//     let maiorNome = nomes[0];
+//    for(let index = 0; index < nomes.length; index+=1){
+//        if(maiorNome.length < nomes[index].length){
+//            maiorNome = nomes[index];
+//        }
+//    }
+//    return maiorNome;
+// }
+// console.log(maisCaracteres(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
+
+//Exercicio 5
+
+// function numeroRepete(numeros) {
+//   let contRepeticao = 0;
+//   let num = 0;
+//   let indice = 0;
+//   for (let index in numeros) {
+//     let contNumero = 0;
+//     num = numeros[index];
+//     for (index2 in numeros) {
+//       if (num === numeros[index2]) {
+//         contNumero += 1;
+//       }
+//     }
+//     if (contNumero > contRepeticao) {
+//       contRepeticao = contNumero;
+//       indice = index;
+//     }
+//   }
+//   return numeros[indice];
+// }
+// console.log(numeroRepete([2, 3, 2, 5, 8, 2, 3, 3, 3]));
+
+//Exercicio 6
+
+// function somatorio(N) {
+//   let resultado = 0;
+//   for (let index = 0; index <= N; index += 1) {
+//     resultado += index;
+//   }
+//   return resultado;
+// }
+// console.log(somatorio(5));
+
+//Exercicio 7
+
+// function final(word, ending){
+//   let wordC = word.split("").reverse().join("");
+//   let wordCortada = wordC.split("", ending.length).reverse().join("");
+//   if (wordCortada === ending) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(final('trybe', 'be'));
+
+
+//BONUS
+
 
