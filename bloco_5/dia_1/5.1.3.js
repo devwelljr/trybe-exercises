5c4fb786f134599345768cb25b6e306f3e73e3b6
Wellington <junior.wel02@exemplo.br>
2021-04-29 16:20:22 -0300

    exercicio parte 3

---
+++ bloco_5/dia_1/5.1.3.js
@@ -0,0 +1,3 @@
+document.getElementById("header-container").style.backgroundColor = "rgb(119,221,119)";
+document.getElementById("footer-container").style.backgroundColor = "rgb(8,77,110)";
+document.getElementsByClassName("emergency-tasks").style.backgroundColor = "rgb(233,150,122)";
