b20fdbc16a94110ab2739ac893b0c2f1e0c511e9
Wellington <junior.wel02@exemplo.br>
2021-05-03 19:10:19 -0300

    1

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -14,3 +14,25 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  //EXERCICIO 1
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  let lista = document.querySelector('#days');
+  for (let index = 0; index < dezDaysList.length; index += 1){
+    let lis = dezDaysList[index];
+    let list = document.createElement("li");
+    list.classList = "day";
+    if(lis == 24 || lis == 31){
+      list.classList = "day holiday";
+    }
+    if(lis == 4 || lis == 11|| lis == 18){
+      list.classList = "friday"
+    }
+    if(lis == 25){
+      list.classList = "day holiday friday"
+    }
+    list.innerText = lis;
+    lista.appendChild(list);
+  }
+  
