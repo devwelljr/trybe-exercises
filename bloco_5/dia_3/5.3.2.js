3e4e4da1c1ee24a6e5b54ec659a1bbfb4d7f0943
Wellington <junior.wel02@exemplo.br>
2021-05-03 16:20:40 -0300

    Ate a 3

--- bloco_5/dia_3/5.3.2.js
+++ bloco_5/dia_3/5.3.2.js
@@ -1,13 +1,27 @@
-const divUm = document.getElementById('divUm');
-const divDois = document.getElementById('divDois');
-const divTres = document.getElementById('divTres');
-const input = document.getElementById('input');
-const myWebpage = document.getElementById('mySpotrybefy');
+const divUm = document.getElementById("divUm");
+const divDois = document.getElementById("divDois");
+const divTres = document.getElementById("divTres");
+const input = document.getElementById("input");
+const myWebpage = document.getElementById("mySpotrybefy");
+
+function addTech(evento) {
+  let elementoSelecionado = document.querySelector('.tech');
+  elementoSelecionado.classList.remove('tech');
+  evento.target.classList.toggle('tech');
+}
+
+divUm.addEventListener('click', addTech);
+divDois.addEventListener('click', addTech);
+divTres.addEventListener('click', addTech);
+
+function AlteraTexto(evento){
+  let elementoSelecionado = document.querySelector('.tech');
+  elementoSelecionado.innerText = evento.target.value;
+}
+
+input.addEventListener('keypress', AlteraTexto);
 
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
@@ -21,12 +35,12 @@ Segue abaixo um exemplo do uso de event.target:
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
+  event.target.innerText = "Opção reiniciada";
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
 
-divUm.addEventListener('dblclick', resetText);
+divUm.addEventListener("dblclick", resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
