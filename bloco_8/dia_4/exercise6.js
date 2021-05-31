b89283c4bceb6636258d6dc7ee3956e85b6214dd
Wellington <junior.wel02@exemplo.br>
2021-05-31 19:29:29 -0300

    6

---
+++ bloco_8/dia_4/exercise6.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
+
+function studentAverage() {
+  let totais = grades.map((elemento) => {
+    return elemento.reduce((acc, number) => acc + number);
+  });
+
+  let medias = totais.map((number) => Math.abs(number / 5));
+
+  let volta = students.map((nome, index) => ({name: nome, average: medias[index]}))
+
+  return volta;
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
