4f5e5bd3286701dd83b9f6162d715d5ad3af8fc0
Wellington <junior.wel02@exemplo.br>
2021-06-08 18:45:05 -0300

    3

---
+++ bloco_9/dia_2/exercise3.js
@@ -0,0 +1,28 @@
+function pro () {
+    const pros = new Promise ((resolve, reject) => {
+      const numeros = [];
+      const somaDividida = [];
+      const divisoes = [2, 3, 5, 10];
+      const gerarNumero = () => Math.trunc(Math.random() * (50 - 1));
+  
+      for (let index = 0; index < 10; index += 1) {
+          numeros.push(gerarNumero());
+      }
+  
+      const quadrado = numeros.map((number) => number ** 2)
+  
+      const soma = quadrado.reduce((acc, actual) => acc += actual);
+      
+      if (soma < 8000) {
+      for (let i = 0; i < divisoes.length; i += 1){
+        somaDividida.push(Math.trunc(soma / divisoes[i]));
+      }
+      resolve(somaDividida);
+      } 
+      reject();
+    });
+    pros
+    .then((arrayResult) => console.log(`Promise resolvida, resultado das divisões: ${arrayResult}`))
+    .catch(() => console.log('Promise rejeitada'));
+  }
+  pro();
