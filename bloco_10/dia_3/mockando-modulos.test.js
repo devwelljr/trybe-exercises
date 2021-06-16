7d5aa6437a238d89108469b94004023a10ddfea8
Wellington <junior.wel02@exemplo.br>
2021-06-16 13:13:16 -0300

    mockando modulos

---
+++ bloco_10/dia_3/mockando-modulos.test.js
@@ -0,0 +1,49 @@
+const service = require('./mockando-modulos.js');
+//npm test mockando-modulos.test.js
+
+//EXEMPLO 1 QUE NÃO FUNCIONA POR NÃO SER UMA SIMULAÇÃO DA FUNÇÃO
+test('#randomRgbColor1', () => {
+  // testando se a função foi chamada
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
+});
+
+//EXEMPLO 2 COM JEST.FN()
+test("#randomRgbColor2", () => {
+  // testando se a função foi chamada. Não simulamos nenhum comportamento aqui, pois, para esse teste, isso não importa! Queremos saber se ela foi chamada e ponto final.
+  service.randomRgbColor = jest.fn();
+
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
+});
+
+//EXEMPLO 3 USANDO MOCKRETURNVALUE()
+test("#randomRgbColor3", () => {
+  // testando se a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest.fn().mockReturnValue("rgb(255, 255, 255)");
+
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
+  expect(service.randomRgbColor()).toBe("rgb(255, 255, 255)");
+});
+
+//EXEMPLO 4 
+test("#randomRgbColor", () => {
+  // testando quantas vezes a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
+
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+  expect(service.randomRgbColor()).toBe("first call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe("second call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe("default value");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
+});
