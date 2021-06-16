53428123daa97d7d0891594511e0251672210ad1
Wellington <junior.wel02@exemplo.br>
2021-06-16 19:21:54 -0300

    4 exercicios

---
+++ bloco_10/dia_3/exercises/4.js
@@ -0,0 +1,7 @@
+const maiusculo = (string) => string.toUpperCase();
+ 
+const primeiraLetra = (string) => string[0];
+
+const juntarStrings = (a, b) => a.concat(b);
+
+module.exports = { maiusculo, primeiraLetra, juntarStrings }
