85e323dc1470f2967f31364a526c818371212f1d
Wellington <junior.wel02@exemplo.br>
2021-06-22 15:32:31 -0300

    propTypes

---
+++ front-end/bloco_11/dia_2/proptypes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeting from './Components/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+     <Greeting name='a' lastName='Gregorio'/>
+    </div>
+  );
+}
+
+export default App;
