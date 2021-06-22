a44fd54781570c0f624550f2e5859af0b8c17a7c
Wellington <junior.wel02@exemplo.br>
2021-06-22 12:49:39 -0300

    inicio dia 2

---
+++ front-end/bloco_11/dia_2/props/src/App.js
@@ -0,0 +1,11 @@
+import Greeting from "./Components/Greeting";
+
+function App() {
+  return (
+    <main>
+      <Greeting name="Wellington" lastName="Gregorio" />
+    </main>
+  );
+}
+
+export default App;
