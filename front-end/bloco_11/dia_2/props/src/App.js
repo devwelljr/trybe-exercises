ec41f9d7a5182bbfc7baac19344576f865d41453
Wellington <junior.wel02@exemplo.br>
2021-06-22 13:06:30 -0300

    props

--- front-end/bloco_11/dia_2/props/src/App.js
+++ front-end/bloco_11/dia_2/props/src/App.js
@@ -1,9 +1,13 @@
 import Greeting from "./Components/Greeting";
+import Image from "./Components/Image";
+import './App.css';
 
 function App() {
   return (
     <main>
       <Greeting name="Wellington" lastName="Gregorio" />
+
+      <Image src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt='Cute cat staring'/>
     </main>
   );
 }
